7722db893670593e7a49f88130e360c0a508f690
Shreeya V   V <113493317+shreeyabdes@users.noreply.github.com>
2022-09-28 04:24:35 +0000

    understood

---
+++ .ccls-cache/@home@runner@c-activity-sets-shreeyabdes-5/set01@problem09.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  float a,s;
+  printf("Enter one number");
+  scanf("%f",&a);
+  s=sqrt(a);
+  printf("square root is %f\n",s);
+  return 0;
+}
