3e0e4bf2b7d8da25ae7259c12ecc0a0e07f7af2d
Shreeya V   V <113493317+shreeyabdes@users.noreply.github.com>
2022-12-22 02:31:38 +0000

    understood

--- .ccls-cache/@home@runner@c-activity-sets-shreeyabdes-77/set02@problem04.c
+++ .ccls-cache/@home@runner@c-activity-sets-shreeyabdes-77/set02@problem04.c
@@ -15,41 +15,33 @@ int find_mood(float radius, float height, float length)
     if (height<radius && height<length);
     mood=0;
     
-    if (length<radius && length<height);
+    if (lenght<radius && lenght<heigth);
     mood=-1;
     
     return mood;
 }
 
-void output(int mood)
+void output(float radius, float height, float length, int mood)
 {
-    if (mood==1)
+    if mood==1;
     printf("The camel id happy");
     
-    else if (mood==0)
+    else if mood==0
     printf("The camel is sick");
     
-    else if (mood==-1)
+    else if mood==-1
     printf("The camel is tense");
 }
 
-int main()
+int main;
 {
     int mood; 
     float radius, lenght, height;
     input_camel_details(&height, &lenght, &radius);
     mood=find_mood(height, lenght, radius);
-    output(mood);
+    output( mood);
     return 0;
 }
-
-
-
-
-
-
-
-
 
 
 
