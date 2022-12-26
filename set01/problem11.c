befe79be3d79915eb0589f747f7ec97133e81d2d
Shreeya V   V <113493317+shreeyabdes@users.noreply.github.com>
2022-12-26 03:08:18 +0000

    understood

--- set01/problem11.c
+++ set01/problem11.c
@@ -1,59 +1,38 @@
-#include<stdio.h>
-struct_complex
+#include <stdio.h>
+struct _complex 
 {
- float real;
- float imaginary;
+	float real;
+	float imaginary;
 };
+typedef struct _complex Complex;
 
-typedef struct complex Complexx;
-Complex input complex()
-
+Complex input_complex()
 {
   Complex c;
-  printf("Enter eal and imaginary arts of complex numbers/n");
-  scanf("%f%f"&c.real,&c.imaginary);
-  return c;
-}
+  printf("Enter real and imaginary parts of complex number\n");
+	scanf("%f%f",&c.real,&c.imaginary);
+	return c;
 
-Compplex add_complex(complex a, complex b)
+}
+Complex add_complex(Complex a, Complex b)
 {
-  Complex c;
-  c.real=a.real+b.real;
-  c.imaginary=a.imaginary+ b .imaginary;
-  retuen c;
-}
 
+	Complex c;
+	c.real = a.real + b.real;
+	c.imaginary = a.imaginary + b.imaginary;
+	return c;
+
+}
 void output(Complex a, Complex b, Complex sum)
 {
-printf("("%f+i%f)+(%f+%f) is(%f+i%f)\n",a.real, a.imaginary,b.real, b. imaginary, sum.real, sum.imaginary);
-  
-c.real=a.real+b.real
-c.imaginary=a.imaginary+b.imaginary
-  return c
-  }
+  printf("(%f + i %f) + (%f + i %f) is (%f + i%f)\n",a.real, a.imaginary, b.real, b.imaginary, sum.real, sum.imaginary); 
 
-Complex add_n_complex(int n, Co c[n])
-{
-  Complex sum={0,0};
-  for(int i=0; i++)
-    sum=add(sum,c[i]);
-  retuen sum;
 }
-
-void output(nt n Complex c[n], Complex sum)
-{
-  for (int i=0; i<n-1; i++)
-     printf("(%f+ i%f)+\n", c[i].real, c[i].imaginary);
-    printf("(%f+i%f=\n"),c[n-1.real], c[n-1].imaginary);
-    printf("(%f+i%f\n", sum.real,sum.imaginary);
-}
-
 int main()
 {
-  int n= get_n();
-  Complex c[n]
-  input_n_comples(n,&c[n]);
-  Complex sum= add_n_complex(n,&c[n]);
-  output(n,&c[n],sum);
-  return 0;
+	Complex a,b,c;
+	a= input_complex();
+	b= input_complex();
+	c= add_complex(a,b);
+	output(a,b,c);
 }
